d5dff762ad2067cf0efa7313914c7ccc5e097f33
Rebeca <rebeca.fonseca@gko.com.br>
2021-10-12 07:10:12 -0300

    fix corretude

--- trab2/corretude.py
+++ trab2/corretude.py
@@ -2,7 +2,7 @@ arquivo = open('arquivo-saida.txt', 'r')
 erro = False
 for linha in arquivo:
     linha = linha.strip()
-    print("linha=",linha)
+    #print("linha=",linha)
     numeros = linha.split(" ")
     # numeros = []
     # for i,num in enumerate(l):
@@ -10,11 +10,16 @@ for linha in arquivo:
     #     x = num[i]
     #     numeros.append(x)
     # print(numeros)
-    for index, num in enumerate(numeros, start=0):
-        
-        if numeros[index-1] >= numeros[index]:
-    #        print(numeros[index-1] ," >= ",numeros[index], "index=", index)
+    #for index, num in enumerate(numeros, start=0):
+    x = len(numeros)
+    index=1
+    while(index<x):
+        num1 = int(numeros[index-1].strip())
+        num2 = int(numeros[index].strip())
+        if num1 > num2:
+            print(numeros[index-1] ,">=",numeros[index], "index=", index)
             erro = True
+        index = index + 1
 if(erro):
     print("Erro no arquivo :(")
 else: 
